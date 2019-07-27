5c54393f02fdb5509a31ec2044291b74b26875c9
lguohan <lguohan@gmail.com>
2019-07-26 22:16:59 -0700

    [vstest]: add --imgname option to specify vs image to test (#996)
    default image is docker-sonic-vs:latest
    
    Signed-off-by: Guohan Lu <gulv@microsoft.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,6 +23,8 @@ def pytest_addoption(parser):
                       help="dvs name")
     parser.addoption("--keeptb", action="store_true", default=False,
                       help="keep testbed after test")
+    parser.addoption("--imgname", action="store", default="docker-sonic-vs",
+                      help="image name")
 
 class AsicDbValidator(object):
     def __init__(self, dvs):
@@ -142,7 +144,7 @@ class VirtualServer(object):
         return subprocess.check_output("ip netns exec %s %s" % (self.nsname, cmd), shell=True)
 
 class DockerVirtualSwitch(object):
-    def __init__(self, name=None, keeptb=False, fakeplatform=None):
+    def __init__(self, name=None, imgname=None, keeptb=False, fakeplatform=None):
         self.basicd = ['redis-server',
                        'rsyslogd']
         self.swssd = ['orchagent',
@@ -214,7 +216,7 @@ class DockerVirtualSwitch(object):
             self.environment = ["fake_platform={}".format(fakeplatform)] if fakeplatform else []
 
             # create virtual switch container
-            self.ctn = self.client.containers.run('docker-sonic-vs', privileged=True, detach=True,
+            self.ctn = self.client.containers.run(imgname, privileged=True, detach=True,
                     environment=self.environment,
                     network_mode="container:%s" % self.ctn_sw.name,
                     volumes={ self.mount: { 'bind': '/var/run/redis', 'mode': 'rw' } })
@@ -794,8 +796,9 @@ class DockerVirtualSwitch(object):
 def dvs(request):
     name = request.config.getoption("--dvsname")
     keeptb = request.config.getoption("--keeptb")
+    imgname = request.config.getoption("--imgname")
     fakeplatform = getattr(request.module, "DVS_FAKE_PLATFORM", None)
-    dvs = DockerVirtualSwitch(name, keeptb, fakeplatform)
+    dvs = DockerVirtualSwitch(name, imgname, keeptb, fakeplatform)
     yield dvs
     if name == None:
         dvs.get_logs(request.module.__name__)
