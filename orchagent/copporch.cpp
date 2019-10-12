80b2ace778f4da1c05d174a83e6fde1476b1e579
jpxjlrldgit <48697498+jpxjlrldgit@users.noreply.github.com>
2019-10-11 19:34:20 -0700

    sonic-swss/orchagent:  Add new protocol trap name support (#1087)
    * Added new protocol trap name support

--- orchagent/copporch.cpp
+++ orchagent/copporch.cpp
@@ -69,7 +69,9 @@ static map<string, sai_hostif_trap_type_t> trap_id_map = {
     {"router_custom_range", SAI_HOSTIF_TRAP_TYPE_ROUTER_CUSTOM_RANGE_BASE},
     {"l3_mtu_error", SAI_HOSTIF_TRAP_TYPE_L3_MTU_ERROR},
     {"ttl_error", SAI_HOSTIF_TRAP_TYPE_TTL_ERROR},
-    {"udld", SAI_HOSTIF_TRAP_TYPE_UDLD}
+    {"udld", SAI_HOSTIF_TRAP_TYPE_UDLD},
+    {"bfd", SAI_HOSTIF_TRAP_TYPE_BFD},
+    {"bfdv6", SAI_HOSTIF_TRAP_TYPE_BFDV6}
 };
 
 static map<string, sai_packet_action_t> packet_action_map = {
