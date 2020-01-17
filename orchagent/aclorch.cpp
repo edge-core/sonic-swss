3e53c3e56f4d7df1e9fed825416cf1b08dd91242
Danny Allen <daall@microsoft.com>
2020-01-16 18:44:20 -0800

    [aclorch] Validate that provided IN/OUT_PORTS are physical interfaces (#1156)
    * [aclorch] Validate that provided IN/OUT_PORTS are physical interfaces
    - Add a check during the rule validation step to make sure provided IN/OUT_PORTS rules only include physical interfaces
    - Add vs test cases for invalid IN/OUT_PORTS inputs
    
    Signed-off-by: Danny Allen <daall@microsoft.com>
    
    * Clarify test names
    
    * Add extra delay for table teardown
    
    * Check if new tests are causing tests to fail
    
    * Check if new tests are causing tests to fail
    
    * Check if ProducerStateTable is having problems
    
    * Make delete ACL table test more strict
    
    * Remove invalid interface test cases
    
    * Undo change to delete test
    
    * Undo change to delete test

--- orchagent/aclorch.cpp
+++ orchagent/aclorch.cpp
@@ -206,6 +206,13 @@ bool AclRule::validateAddMatch(string attr_name, string attr_value)
                     SWSS_LOG_ERROR("Failed to locate port %s", alias.c_str());
                     return false;
                 }
+
+                if (port.m_type != Port::PHY)
+                {
+                    SWSS_LOG_ERROR("Cannot bind rule to %s: IN_PORTS can only match physical interfaces", alias.c_str());
+                    return false;
+                }
+
                 m_inPorts.push_back(port.m_port_id);
             }
 
@@ -230,6 +237,13 @@ bool AclRule::validateAddMatch(string attr_name, string attr_value)
                     SWSS_LOG_ERROR("Failed to locate port %s", alias.c_str());
                     return false;
                 }
+
+                if (port.m_type != Port::PHY)
+                {
+                    SWSS_LOG_ERROR("Cannot bind rule to %s: OUT_PORTS can only match physical interfaces", alias.c_str());
+                    return false;
+                }
+
                 m_outPorts.push_back(port.m_port_id);
             }
 
