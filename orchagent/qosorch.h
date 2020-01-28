5eb1ea3267deac7287cc5817d1e6239fa7cca1d0
zhenggen-xu <zxu@linkedin.com>
2020-01-28 10:15:04 -0800

    [qosorch]: Remove Init Color ACLs (#1084)
    InitSystemAcl table is removed.
    ECN ACL rules: DSCP 0/8 mapping to yellow are removed.
    
    Signed-off-by: Zhenggen Xu <zxu@linkedin.com>

--- orchagent/qosorch.h
+++ orchagent/qosorch.h
@@ -134,11 +134,6 @@ private:
     typedef map<string, qos_table_handler> qos_table_handler_map;
     typedef pair<string, qos_table_handler> qos_handler_pair;
 
-    void initColorAcl();
-    sai_object_id_t initSystemAclTable();
-    void initAclEntryForEcn(sai_object_id_t acl_table_id, sai_uint32_t priority,
-                            sai_uint8_t ecn_field, sai_uint8_t dscp_field, sai_int32_t color);
-
     void initTableHandlers();
 
     task_process_status handleDscpToTcTable(Consumer& consumer);
