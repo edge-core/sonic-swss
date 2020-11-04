1c68706249815af48b5315212d9cfc990a6a803b
Volodymyr Samotiy <volodymyrs@nvidia.com>
2020-11-04 21:34:46 +0200

    [bitmap_vnet] Remove BMTOR implementation (#1496)
    Signed-off-by: Volodymyr Samotiy <volodymyrs@nvidia.com>

--- orchagent/orchdaemon.cpp
+++ orchagent/orchdaemon.cpp
@@ -102,14 +102,8 @@ bool OrchDaemon::init()
     };
 
     VNetOrch *vnet_orch;
-    if (platform == MLNX_PLATFORM_SUBSTRING)
-    {
-        vnet_orch = new VNetOrch(m_applDb, APP_VNET_TABLE_NAME, VNET_EXEC::VNET_EXEC_BRIDGE);
-    }
-    else
-    {
-        vnet_orch = new VNetOrch(m_applDb, APP_VNET_TABLE_NAME);
-    }
+    vnet_orch = new VNetOrch(m_applDb, APP_VNET_TABLE_NAME);
+
     gDirectory.set(vnet_orch);
     VNetCfgRouteOrch *cfg_vnet_rt_orch = new VNetCfgRouteOrch(m_configDb, m_applDb, cfg_vnet_tables);
     gDirectory.set(cfg_vnet_rt_orch);
