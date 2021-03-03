94970ac6759954dbe12ce3ae630a7a30a2fab3b5
gord_chen <gord_chen@edge-core.com>
2021-03-03 09:30:40 +0000

    [vxlanorch] Add safety checks for VTEP management and EVPN interaction
    This patch adds multiple protections to prevent invalid memory access and ensure the correct removal order of VTEPs and related resources:
    
    1. Fix EVPN orch accessing invalid memory
    EVPN orch stores a private pointer to the local VTEP, which is managed by the VxLAN tunnel orch. When the VxLAN tunnel orch removes the local VTEP, it does not notify EVPN orch, leading to potential use-after-free issues. This patch adds protection to avoid accessing invalid addresses.
    
    2. Ensure local VTEP is the last tunnel removed
    Adds a check to guarantee that the local VTEP is removed only after all user tunnels. EVPN orch also relies on this behavior to avoid processing errors due to unexpected tunnel removal order.
    
    3. Check VLAN-VNI map before removing local VTEP
    Adds a validation step to ensure the VLAN-VNI map is cleared before removing the local VTEP. This prevents premature deletion of shared resources that may still be in use by user-defined tunnels.

--- orchagent/vxlanorch.h
+++ orchagent/vxlanorch.h
@@ -274,6 +274,11 @@ public:
         return vxlan_tunnel_table_.at(tunnelName).get();
     }
 
+    auto getVxlanTunnelSize()
+    {
+        return vxlan_tunnel_table_.size();
+    }
+
     bool addTunnel(const std::string tunnel_name,VxlanTunnel* tnlptr)
     {
        vxlan_tunnel_table_[tunnel_name] = (VxlanTunnel_T)tnlptr;
@@ -344,6 +349,11 @@ public:
         {
             return 0;
         }
+    }
+
+    auto getVniVlanMapTableSize()
+    {
+        return vxlan_vni_vlan_map_table_.size();
     }
 
     void addVlanMappedToVni(uint32_t vni, uint16_t vlan_id)
@@ -541,6 +551,11 @@ public:
     VxlanTunnel* getEVPNVtep() 
     { 
         return source_vtep_ptr;
+    }
+
+    void delEVPNVtep()
+    {
+        source_vtep_ptr = NULL;
     }
 
 private:
