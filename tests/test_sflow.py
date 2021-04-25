a33960ca0451ab2195caa9ac04fed3ed7bb5c230
Vivek Reddy <vivekreddykarri98@gmail.com>
2021-04-25 00:43:15 -0400

    [SflowMgr] SamplingRate Update by Speed Change Added (#1721)
    Currently, the SflowMgr::sflowUpdatePortInfo method updates the sampling-rate only when adding a new-port. Updated the method to be active for speed change notifications all the time.
    
    Co-authored-by: Vivek Reddy Karri <vkarri@nvidia.com>

--- tests/test_sflow.py
+++ tests/test_sflow.py
@@ -1,3 +1,5 @@
+import time
+
 class TestSflow:
     speed_rate_table = {
         "400000": "400000",
@@ -131,6 +133,48 @@ class TestSflow:
 
         expected_fields = {"SAI_SAMPLEPACKET_ATTR_SAMPLE_RATE": rate}
         self.adb.wait_for_field_match("ASIC_STATE:SAI_OBJECT_TYPE_SAMPLEPACKET", sample_session, expected_fields)
+    
+    def test_SamplingRatePortCfgUpdate(self, dvs, testlog):
+        '''
+        This test checks if the SflowMgr updates the sampling rate 
+        1) When the Speed is Updated on the port and no local configuration has been given on the port
+        Eg:
+        config sflow enable
+        config interface speed Ethernet0  25000  (Let's suppose Original Speed for Ethernet0 is 100G)
+        show sflow interface | grep Ethernet0    (Should see a sampling rate of 25000 not 100000)
+        '''
+        self.setup_sflow(dvs)
+        appldb = dvs.get_app_db()
+        #dvs.runcmd("portconfig -p {} -s {}".format("Ethernet0", "25000"))
+        self.cdb.update_entry("PORT", "Ethernet0", {'speed' : "25000"})
+        expected_fields = {"sample_rate": self.speed_rate_table["25000"]}
+        appldb.wait_for_field_match("SFLOW_SESSION_TABLE", "Ethernet0", expected_fields)
+
+    
+    def test_SamplingRateManualUpdate(self, dvs, testlog):
+        '''  
+        This test checks if the SflowMgr updates the sampling rate 
+        1) When the Cfg Sflow Table is updated with sampling rate by the user, this rate should not be impacted by Port Speed Changes
+        Eg:
+        config sflow enable
+        config sflow interface sample-rate Ethernet4 256
+        config interface Ethernet0 speed 25000  (Original Speed for Ethernet0 is 100G)
+        show sflow interface | grep Ethernet0   (Should see  a sampling rate of 256 not 100000 or 25000
+        '''
+        self.setup_sflow(dvs)
+        appldb = dvs.get_app_db()
+        
+        session_params = {"admin_state": "up", "sample_rate": "256"}
+        self.cdb.create_entry("SFLOW_SESSION", "Ethernet4", session_params)
+        self.cdb.wait_for_field_match("SFLOW_SESSION", "Ethernet4", session_params)
+        appldb.wait_for_field_match("SFLOW_SESSION_TABLE", "Ethernet4", {"sample_rate": "256"})
+        
+        self.cdb.update_entry("PORT", "Ethernet4", {'speed' : "25000"})
+        # The Check here is about the original value not getting changed. 
+        # If some bug was to appear, let's give it some time to get noticed
+        time.sleep(1) 
+        appldb.wait_for_field_match("SFLOW_SESSION_TABLE", "Ethernet4", {"sample_rate": "256"})
+    
 
     def test_Teardown(self, dvs, testlog):
         self.setup_sflow(dvs)
