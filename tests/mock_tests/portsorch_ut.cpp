189ee07510918494892ffc57c0b88b26af9ebf6d
shlomibitton <60430976+shlomibitton@users.noreply.github.com>
2021-07-02 00:39:29 +0300

    [flex-counters] Delay flex counters stats init for faster boot time (#1803)
    What I did
    Update flex counters DB with counters stats only when counters are enabled.
    As long as the polling counters are not enabled, flex counters information will stored internally on PortsOrch.
    
    Why I did it
    Creating flex counters objects on the DB will trigger 'SYNCD' to access the HW for query statistics capabilities.
    This HW access takes time and will be better to finish boot before doing this (mainly for fast-reboot but good to have in general).
    The flex counters are not crucial at boot time, we can delay it to the end of the boot process.
    
    How I verified it
    Reboot a switch and observer the flex counters DB populated after counters are enabled.
    
    Signed-off-by: Shlomi Bitton <shlomibi@nvidia.com>

--- tests/mock_tests/portsorch_ut.cpp
+++ tests/mock_tests/portsorch_ut.cpp
@@ -146,8 +146,13 @@ namespace portsorch_test
 
         ASSERT_EQ(gPortsOrch, nullptr);
 
-        gPortsOrch = new PortsOrch(m_app_db.get(), m_state_db.get(), ports_tables, m_chassis_app_db.get());
+        vector<string> flex_counter_tables = {
+            CFG_FLEX_COUNTER_TABLE_NAME
+        };
+        auto* flexCounterOrch = new FlexCounterOrch(m_config_db.get(), flex_counter_tables);
+        gDirectory.set(flexCounterOrch);
 
+        gPortsOrch = new PortsOrch(m_app_db.get(), m_state_db.get(), ports_tables, m_chassis_app_db.get());
         vector<string> buffer_tables = { APP_BUFFER_POOL_TABLE_NAME,
                                          APP_BUFFER_PROFILE_TABLE_NAME,
                                          APP_BUFFER_QUEUE_TABLE_NAME,
