20c740c9371507e6827200276761617bcfa526dd
iris_hsu <iris_hsu@edge-core.com>
2022-03-18 01:56:07 +0000

    Set tunnel id to portOidToName for support update tunnel status.

--- orchagent/portsorch.cpp
+++ orchagent/portsorch.cpp
@@ -6754,6 +6754,7 @@ bool PortsOrch::addTunnel(string tunnel_alias, sai_object_id_t tunnel_id, bool h
         tunnel.m_learn_mode = SAI_BRIDGE_PORT_FDB_LEARNING_MODE_DISABLE;
     }
     m_portList[tunnel_alias] = tunnel;
+    saiOidToAlias[tunnel_id] = tunnel_alias;
 
     SWSS_LOG_INFO("addTunnel:: %" PRIx64, tunnel_id);
 
@@ -6764,6 +6765,7 @@ bool PortsOrch::removeTunnel(Port tunnel)
 {
     SWSS_LOG_ENTER();
 
+    saiOidToAlias.erase(tunnel.m_tunnel_id);
     m_portList.erase(tunnel.m_alias);
 
     return true;
@@ -7628,6 +7630,11 @@ void PortsOrch::updatePortOperStatus(Port &port, sai_port_oper_status_t status)
     if (status == port.m_oper_status)
     {
         return;
+    }
+
+    if (port.m_type == Port::PHY || port.m_type == Port::TUNNEL)
+    {
+        updateDbPortOperStatus(port, status);
     }
 
     if (port.m_type == Port::PHY)
