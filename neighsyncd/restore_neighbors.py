c1eb99a7a92912877e17cb1bd0060726a5c2ecfb
Vivek <vkarri@nvidia.com>
2022-09-12 16:48:02 -0700

    check state_db for po before sending ARP/ND pkts (#2444)
    Signed-off-by: Vivek Reddy Karri <vkarri@nvidia.com>
    Check STATE_DB before sending ARP/ND pkts for neighbors associated with PortChannel. As a part of intf check, wait for the LAG_MEMBER_TABLE to be populated for Portchannels ifaces

--- neighsyncd/restore_neighbors.py
+++ neighsyncd/restore_neighbors.py
@@ -80,20 +80,28 @@ def is_intf_oper_state_up(intf):
         return True
     return False
 
+def check_state_db(intf, db):
+    table_name = ''
+    if 'Vlan' in intf:
+        table_name = 'VLAN_MEMBER_TABLE|{}|*'.format(intf)
+    elif 'PortChannel' in intf:
+        table_name = 'LAG_MEMBER_TABLE|{}|*'.format(intf)
+    else:
+        return True
+    key = db.keys(db.STATE_DB, table_name)
+    if key is None:
+        log_info ("members for {} are not yet created".format(intf))
+        return False
+    if is_intf_up.counter == 0:
+        time.sleep(3*CHECK_INTERVAL)
+        is_intf_up.counter = 1
+    log_info ("intf {} is up".format(intf))
+    return True
+
 def is_intf_up(intf, db):
     if not is_intf_oper_state_up(intf):
          return False
-    if 'Vlan' in intf:
-        table_name = 'VLAN_MEMBER_TABLE|{}|*'.format(intf)
-        key = db.keys(db.STATE_DB, table_name)
-        if key is None:
-            log_info ("Vlan member is not yet created")
-            return False
-        if is_intf_up.counter == 0:
-            time.sleep(3*CHECK_INTERVAL)
-            is_intf_up.counter = 1
-        log_info ("intf {} is up".format(intf))
-    return True
+    return check_state_db(intf, db)
 
 # read the neigh table from AppDB to memory, format as below
 # build map as below, this can efficiently access intf and family groups later
