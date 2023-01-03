3ce7eb4795ca801b634e39f6c79d6732fb0017b6
Andriy Yurkiv <70649192+ayurkiv-nvda@users.noreply.github.com>
2023-01-03 19:18:58 +0200

    [202205][dual-tor] use 'egress' port for Mellanox platform for ACL rules for standby ToR (#2565)
    - What I did
    For Dual-Tor scenario on standby ports drop packets on the egress stage for mellanox platform only.
    Remain old behavior for other platform.
    
    NOTE
    Currently, it is short-term solution which will be replaced with more generic one
    
    - Why I did it
    Current SAI ACL implementation drops both data plane and control-plane traffic if rules are installed on ingress stage.
    But DualToR expects only data-plane traffic to be dropped, and control-plane should not be affected.
    In order to handle this limitation, we will allow traffic to go into ingress port, then handle control plane traffic and drop that traffic on egress ports
    
    - How I verified it
    Setup Dual-ToR, both Active and Standby should have "healthy" status
    
    Signed-off-by: Andriy Yurkiv <ayurkiv@nvidia.com>

--- orchagent/muxorch.cpp
+++ orchagent/muxorch.cpp
@@ -49,6 +49,13 @@ extern sai_router_interface_api_t* sai_router_intfs_api;
 #define MUX_HW_STATE_UNKNOWN "unknown"
 #define MUX_HW_STATE_ERROR "error"
 
+
+static inline bool isIngressAcl()
+{
+    string platform = getenv("platform") ? getenv("platform") : "";
+    return platform != MLNX_PLATFORM_SUBSTRING;
+}
+
 const map<std::pair<MuxState, MuxState>, MuxStateChange> muxStateTransition =
 {
     { { MuxState::MUX_STATE_INIT, MuxState::MUX_STATE_ACTIVE}, MuxStateChange::MUX_STATE_INIT_ACTIVE
@@ -785,7 +792,7 @@ MuxAclHandler::MuxAclHandler(sai_object_id_t port, string alias)
     SWSS_LOG_ENTER();
 
     // There is one handler instance per MUX port
-    string table_name = MUX_ACL_TABLE_NAME;
+    string table_name = isIngressAcl() ? MUX_ACL_TABLE_NAME : EGRESS_TABLE_DROP;
     string rule_name = MUX_ACL_RULE_NAME;
 
     port_ = port;
@@ -823,7 +830,7 @@ MuxAclHandler::MuxAclHandler(sai_object_id_t port, string alias)
 MuxAclHandler::~MuxAclHandler(void)
 {
     SWSS_LOG_ENTER();
-    string table_name = MUX_ACL_TABLE_NAME;
+    string table_name = isIngressAcl() ? MUX_ACL_TABLE_NAME : EGRESS_TABLE_DROP;
     string rule_name = MUX_ACL_RULE_NAME;
 
     SWSS_LOG_NOTICE("Un-Binding port %" PRIx64 "", port_);
@@ -869,7 +876,7 @@ void MuxAclHandler::createMuxAclTable(sai_object_id_t port, string strTable)
     auto dropType = gAclOrch->getAclTableType(TABLE_TYPE_DROP);
     assert(dropType);
     acl_table.validateAddType(*dropType);
-    acl_table.stage = ACL_STAGE_INGRESS;
+    acl_table.stage = isIngressAcl() ? ACL_STAGE_INGRESS : ACL_STAGE_EGRESS;
     gAclOrch->addAclTable(acl_table);
     bindAllPorts(acl_table);
 }
