1540161c9596f658502251e3eb73312d57d4af75
Baorong Liu <96146196+baorliu@users.noreply.github.com>
2023-03-07 10:54:25 -0800

    [bfdorch] add default TOS value for BFD packet (#2692)
    Cherry-picking #2689

--- orchagent/bfdorch.cpp
+++ orchagent/bfdorch.cpp
@@ -14,6 +14,8 @@ using namespace swss;
 #define BFD_SESSION_DEFAULT_TX_INTERVAL 1000
 #define BFD_SESSION_DEFAULT_RX_INTERVAL 1000
 #define BFD_SESSION_DEFAULT_DETECT_MULTIPLIER 10
+// TOS: default 6-bit DSCP value 48, default 2-bit ecn value 0. 48<<2 = 192
+#define BFD_SESSION_DEFAULT_TOS 192
 #define BFD_SESSION_MILLISECOND_TO_MICROSECOND 1000
 #define BFD_SRCPORTINIT 49152
 #define BFD_SRCPORTMAX 65536
@@ -243,6 +245,7 @@ bool BfdOrch::create_bfd_session(const string& key, const vector<FieldValueTuple
     uint32_t tx_interval = BFD_SESSION_DEFAULT_TX_INTERVAL;
     uint32_t rx_interval = BFD_SESSION_DEFAULT_RX_INTERVAL;
     uint8_t multiplier = BFD_SESSION_DEFAULT_DETECT_MULTIPLIER;
+    uint8_t tos = BFD_SESSION_DEFAULT_TOS;
     bool multihop = false;
     MacAddress dst_mac;
     bool dst_mac_provided = false;
@@ -290,6 +293,10 @@ bool BfdOrch::create_bfd_session(const string& key, const vector<FieldValueTuple
         {
             dst_mac = MacAddress(value);
             dst_mac_provided = true;
+        }
+        else if (fvField(i) == "tos")
+        {
+            tos = to_uint<uint8_t>(value);
         }
         else
             SWSS_LOG_ERROR("Unsupported BFD attribute %s\n", fvField(i).c_str());
@@ -351,6 +358,10 @@ bool BfdOrch::create_bfd_session(const string& key, const vector<FieldValueTuple
     attr.value.u8 = multiplier;
     attrs.emplace_back(attr);
     fvVector.emplace_back("multiplier", to_string(multiplier));
+
+    attr.id = SAI_BFD_SESSION_ATTR_TOS;
+    attr.value.u8 = tos;
+    attrs.emplace_back(attr);
 
     if (multihop)
     {
