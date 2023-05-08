415bf7b4157fc6b9aabcebad99058acf73d4f350
prabhataravind <108555774+prabhataravind@users.noreply.github.com>
2023-05-08 09:28:11 -0700

    [DASH]: Miscellaneous bug fixes and adding vstests (#2745)
    * [DASH]: dashrouteorch: Change routing action "route_direct" to "direct"
    * [DASH]: Add vnet vstest cases
    * [DASH]: dashorch: Support ENI state changes
    * [DASH]: orchagent: Add PA refcount logic
    * [DASH]: Add refcounting logic to support mappings where multiple CAs map to one PA
    * [DASH]: Reduced logging level to avoid dropped messages
    * [DASH]: Fixed setEniAdminState to add new ENI state to ENI table
    
    Signed-off-by: Prabhat Aravind <paravind@microsoft.com>

--- orchagent/dash/dashvnetorch.cpp
+++ orchagent/dash/dashvnetorch.cpp
@@ -81,7 +81,7 @@ bool DashVnetOrch::addVnetPost(const string& vnet_name, const DashVnetBulkContex
     VnetEntry entry = { id, ctxt.guid };
     vnet_table_[vnet_name] = entry;
     gVnetNameToId[vnet_name] = id;
-    SWSS_LOG_NOTICE("Vnet entry added for %s", vnet_name.c_str());
+    SWSS_LOG_INFO("Vnet entry added for %s", vnet_name.c_str());
 
     return true;
 }
@@ -136,7 +136,7 @@ bool DashVnetOrch::removeVnetPost(const string& vnet_name, const DashVnetBulkCon
     }
     vnet_table_.erase(vnet_name);
     gVnetNameToId.erase(vnet_name);
-    SWSS_LOG_NOTICE("Vnet entry removed for %s", vnet_name.c_str());
+    SWSS_LOG_INFO("Vnet entry removed for %s", vnet_name.c_str());
 
     return true;
 }
@@ -305,12 +305,28 @@ void DashVnetOrch::addPaValidation(const string& key, VnetMapBulkContext& ctxt)
 {
     SWSS_LOG_ENTER();
 
+    auto& object_statuses = ctxt.pa_validation_object_statuses;
+    string pa_ref_key = ctxt.vnet_name + ":" + ctxt.underlay_ip.to_string();
+    auto it = pa_refcount_table_.find(pa_ref_key);
+    if (it != pa_refcount_table_.end())
+    {
+        /*
+         * PA validation entry already exisits. Just increment refcount and add
+         * a dummy success status to satisfy postop
+         */
+        object_statuses.emplace_back(SAI_STATUS_SUCCESS);
+        pa_refcount_table_[pa_ref_key]++;
+        SWSS_LOG_INFO("Increment PA refcount to %u for PA IP %s",
+                        pa_refcount_table_[pa_ref_key],
+                        ctxt.underlay_ip.to_string().c_str());
+        return;
+    }
+
     uint32_t attr_count = 1;
     sai_pa_validation_entry_t pa_validation_entry;
     pa_validation_entry.vnet_id = gVnetNameToId[ctxt.vnet_name];
     pa_validation_entry.switch_id = gSwitchId;
     swss::copy(pa_validation_entry.sip, ctxt.underlay_ip);
-    auto& object_statuses = ctxt.pa_validation_object_statuses;
     sai_attribute_t pa_validation_attr;
 
     pa_validation_attr.id = SAI_PA_VALIDATION_ENTRY_ATTR_ACTION;
@@ -319,6 +335,9 @@ void DashVnetOrch::addPaValidation(const string& key, VnetMapBulkContext& ctxt)
     object_statuses.emplace_back();
     pa_validation_bulker_.create_entry(&object_statuses.back(), &pa_validation_entry,
             attr_count, &pa_validation_attr);
+    pa_refcount_table_[pa_ref_key] = 1;
+    SWSS_LOG_INFO("Initialize PA refcount to 1 for PA IP %s",
+                    ctxt.underlay_ip.to_string().c_str());
 }
 
 bool DashVnetOrch::addVnetMap(const string& key, VnetMapBulkContext& ctxt)
@@ -340,8 +359,10 @@ bool DashVnetOrch::addVnetMap(const string& key, VnetMapBulkContext& ctxt)
         }
         return false;
     }
-    // If the VNET map is already added, don't add it to the bulker and
-    // return true so it's removed from the consumer 
+    /*
+     * If the VNET map is already added, don't add it to the bulker and
+     * return true so it's removed from the consumer
+     */
     return true;
 }
 
@@ -372,7 +393,7 @@ bool DashVnetOrch::addOutboundCaToPaPost(const string& key, const VnetMapBulkCon
             return parseHandleSaiStatusFailure(handle_status);
         }
     }
-    SWSS_LOG_NOTICE("Outbound CA to PA  map entry for %s added", key.c_str());
+    SWSS_LOG_INFO("Outbound CA to PA  map entry for %s added", key.c_str());
 
     return true;
 }
@@ -388,9 +409,12 @@ bool DashVnetOrch::addPaValidationPost(const string& key, const VnetMapBulkConte
     }
 
     auto it_status = object_statuses.begin();
+    string pa_ref_key = ctxt.vnet_name + ":" + ctxt.underlay_ip.to_string();
     sai_status_t status = *it_status++;
     if (status != SAI_STATUS_SUCCESS)
     {
+        /* PA validation entry add failed. Remove PA refcount entry */
+        pa_refcount_table_.erase(pa_ref_key);
         if (status == SAI_STATUS_ITEM_ALREADY_EXISTS)
         {
             // Retry if item exists in the bulker
@@ -404,7 +428,7 @@ bool DashVnetOrch::addPaValidationPost(const string& key, const VnetMapBulkConte
             return parseHandleSaiStatusFailure(handle_status);
         }
     }
-    SWSS_LOG_NOTICE("PA validation entry for %s added", key.c_str());
+    SWSS_LOG_INFO("PA validation entry for %s added", key.c_str());
 
     return true;
 }
@@ -416,7 +440,7 @@ bool DashVnetOrch::addVnetMapPost(const string& key, const VnetMapBulkContext& c
     bool status = addOutboundCaToPaPost(key, ctxt) && addPaValidationPost(key, ctxt);
     if (!status)
     {
-        SWSS_LOG_ERROR("addVnetMapPost return due to status false for %s ", key.c_str());
+        SWSS_LOG_ERROR("addVnetMapPost failed for %s ", key.c_str());
         return false;
     }
 
@@ -424,7 +448,7 @@ bool DashVnetOrch::addVnetMapPost(const string& key, const VnetMapBulkContext& c
     VnetMapEntry entry = {  gVnetNameToId[vnet_name], ctxt.routing_type, ctxt.dip, ctxt.underlay_ip,
         ctxt.mac_address, ctxt.metering_bucket, ctxt.use_dst_vni };
     vnet_map_table_[key] = entry;
-    SWSS_LOG_NOTICE("Vnet map added for %s", key.c_str());
+    SWSS_LOG_INFO("Vnet map added for %s", key.c_str());
 
     return true;
 }
@@ -448,13 +472,41 @@ void DashVnetOrch::removePaValidation(const string& key, VnetMapBulkContext& ctx
     SWSS_LOG_ENTER();
 
     auto& object_statuses = ctxt.pa_validation_object_statuses;
-    sai_pa_validation_entry_t pa_validation_entry;
-    pa_validation_entry.vnet_id = vnet_map_table_[key].dst_vnet_id;
-    pa_validation_entry.switch_id = gSwitchId;
-    swss::copy(pa_validation_entry.sip, vnet_map_table_[key].underlay_ip);
+    string underlay_ip = vnet_map_table_[key].underlay_ip.to_string();
+    string pa_ref_key = ctxt.vnet_name + ":" + underlay_ip;
+    auto it = pa_refcount_table_.find(pa_ref_key);
+    if (it == pa_refcount_table_.end())
+    {
+        return;
+    }
+    else
+    {
+        if (--pa_refcount_table_[pa_ref_key] > 0)
+        {
+            /*
+             * PA validation entry already exisits. Just decrement refcount and add
+             * a dummy success status to satisfy postop
+             */
+            object_statuses.emplace_back(SAI_STATUS_SUCCESS);
+            SWSS_LOG_INFO("Decrement PA refcount to %u for PA IP %s",
+                            pa_refcount_table_[pa_ref_key],
+                            underlay_ip.c_str());
+            return;
+        }
+        else
+        {
+            sai_pa_validation_entry_t pa_validation_entry;
+            pa_validation_entry.vnet_id = vnet_map_table_[key].dst_vnet_id;
+            pa_validation_entry.switch_id = gSwitchId;
+            swss::copy(pa_validation_entry.sip, vnet_map_table_[key].underlay_ip);
 
-    object_statuses.emplace_back();
-    pa_validation_bulker_.remove_entry(&object_statuses.back(), &pa_validation_entry);
+            object_statuses.emplace_back();
+            pa_validation_bulker_.remove_entry(&object_statuses.back(), &pa_validation_entry);
+            SWSS_LOG_INFO("PA refcount refcount is zero for PA IP %s, removing refcount table entry",
+                            underlay_ip.c_str());
+            pa_refcount_table_.erase(pa_ref_key);
+        }
+    }
 }
 
 bool DashVnetOrch::removeVnetMap(const string& key, VnetMapBulkContext& ctxt)
@@ -464,7 +516,7 @@ bool DashVnetOrch::removeVnetMap(const string& key, VnetMapBulkContext& ctxt)
     bool exists = (vnet_map_table_.find(key) != vnet_map_table_.end());
     if (!exists)
     {
-        SWSS_LOG_WARN("Failed to find vnet mapping %s to remove", key.c_str());
+        SWSS_LOG_INFO("Failed to find vnet mapping %s to remove", key.c_str());
         return true;
     }
 
@@ -501,7 +553,7 @@ bool DashVnetOrch::removeOutboundCaToPaPost(const string& key, const VnetMapBulk
             return parseHandleSaiStatusFailure(handle_status);
         }
     }
-    SWSS_LOG_NOTICE("Outbound CA to PA map entry for %s removed", key.c_str());
+    SWSS_LOG_INFO("Outbound CA to PA map entry for %s removed", key.c_str());
 
     return true;
 }
@@ -510,6 +562,8 @@ bool DashVnetOrch::removePaValidationPost(const string& key, const VnetMapBulkCo
 {
     SWSS_LOG_ENTER();
 
+    string underlay_ip = vnet_map_table_[key].underlay_ip.to_string();
+    string pa_ref_key = ctxt.vnet_name + ":" + underlay_ip;
     const auto& object_statuses = ctxt.pa_validation_object_statuses;
     if (object_statuses.empty())
     {
@@ -533,7 +587,8 @@ bool DashVnetOrch::removePaValidationPost(const string& key, const VnetMapBulkCo
             return parseHandleSaiStatusFailure(handle_status);
         }
     }
-    SWSS_LOG_NOTICE("PA validation entry for %s removed", key.c_str());
+
+    SWSS_LOG_INFO("PA validation entry for %s removed", key.c_str());
 
     return true;
 }
@@ -548,7 +603,7 @@ bool DashVnetOrch::removeVnetMapPost(const string& key, const VnetMapBulkContext
         return false;
     }
     vnet_map_table_.erase(key);
-    SWSS_LOG_NOTICE("Vnet map removed for %s", key.c_str());
+    SWSS_LOG_INFO("Vnet map removed for %s", key.c_str());
 
     return true;
 }
---
+++ tests/test_dash_vnet.py
@@ -0,0 +1,283 @@
+from swsscommon import swsscommon
+import typing
+import time
+
+def to_string(value):
+    if isinstance(value, bool):
+        return "true" if value else "false"
+    return str(value)
+
+
+class ProduceStateTable(object):
+    def __init__(self, database, table_name: str):
+        self.table = swsscommon.ProducerStateTable(
+            database.db_connection,
+            table_name)
+
+    def __setitem__(self, key: str, pairs: typing.Union[dict, list, tuple]):
+        pairs_str = []
+        if isinstance(pairs, dict):
+            pairs = pairs.items()
+        for k, v in pairs:
+            pairs_str.append((to_string(k), to_string(v)))
+        self.table.set(key, pairs_str)
+
+    def __delitem__(self, key: str):
+        self.table.delete(str(key))
+
+
+class Table(object):
+    def __init__(self, database, table_name: str):
+        self.table_name = table_name
+        self.table = swsscommon.Table(database.db_connection, self.table_name)
+
+    def __getitem__(self, key: str):
+        exists, result = self.table.get(str(key))
+        if not exists:
+            return None
+        else:
+            return dict(result)
+
+    def get_keys(self):
+        return self.table.getKeys()
+
+    def get_newly_created_oid(self, old_oids):
+        new_oids = self.asic_db.wait_for_n_keys(table, len(old_oids) + 1)
+        oid = [ids for ids in new_oids if ids not in old_oids]
+        return oid[0]
+
+
+class Dash(object):
+    def __init__(self, dvs):
+        self.dvs = dvs
+        self.app_dash_appliance_table = ProduceStateTable(
+            self.dvs.get_app_db(), "DASH_APPLIANCE_TABLE")
+        self.asic_direction_lookup_table = Table(
+            self.dvs.get_asic_db(), "ASIC_STATE:SAI_OBJECT_TYPE_DIRECTION_LOOKUP_ENTRY")
+        self.asic_vip_table = Table(
+            self.dvs.get_asic_db(), "ASIC_STATE:SAI_OBJECT_TYPE_VIP_ENTRY")
+        self.app_dash_vnet_table = ProduceStateTable(
+            self.dvs.get_app_db(), "DASH_VNET_TABLE")
+        self.asic_dash_vnet_table = Table(
+            self.dvs.get_asic_db(), "ASIC_STATE:SAI_OBJECT_TYPE_VNET")
+        self.app_dash_eni_table = ProduceStateTable(
+            self.dvs.get_app_db(), "DASH_ENI_TABLE")
+        self.asic_eni_table = Table(
+            self.dvs.get_asic_db(), "ASIC_STATE:SAI_OBJECT_TYPE_ENI")
+        self.asic_eni_ether_addr_map_table = Table(
+            self.dvs.get_asic_db(), "ASIC_STATE:SAI_OBJECT_TYPE_ENI_ETHER_ADDRESS_MAP_ENTRY")
+        self.app_dash_vnet_map_table = ProduceStateTable(
+            self.dvs.get_app_db(), "DASH_VNET_MAPPING_TABLE")
+        self.asic_dash_outbound_ca_to_pa_table = Table(
+            self.dvs.get_asic_db(), "ASIC_STATE:SAI_OBJECT_TYPE_OUTBOUND_CA_TO_PA_ENTRY")
+        self.asic_pa_validation_table = Table(
+            self.dvs.get_asic_db(), "ASIC_STATE:SAI_OBJECT_TYPE_PA_VALIDATION_ENTRY")
+        self.app_dash_route_table = ProduceStateTable(
+            self.dvs.get_app_db(), "DASH_ROUTE_TABLE")
+        self.app_dash_route_rule_table = ProduceStateTable(
+            self.dvs.get_app_db(), "DASH_ROUTE_RULE_TABLE")
+        self.asic_outbound_routing_table = Table(
+            self.dvs.get_asic_db(), "ASIC_STATE:SAI_OBJECT_TYPE_OUTBOUND_ROUTING_ENTRY")
+        self.asic_inbound_routing_rule_table = Table(
+            self.dvs.get_asic_db(), "ASIC_STATE:SAI_OBJECT_TYPE_INBOUND_ROUTING_ENTRY")
+
+    def create_appliance(self, appliance_id, attr_maps: dict):
+        self.app_dash_appliance_table[str(appliance_id)] = attr_maps
+
+    def remove_appliance(self, appliance_id):
+        del self.app_dash_appliance_table[str(appliance_id)]
+
+    def create_vnet(self, vnet, attr_maps: dict):
+        self.app_dash_vnet_table[str(vnet)] = attr_maps
+
+    def remove_vnet(self, vnet):
+        del self.app_dash_vnet_table[str(vnet)]
+
+    def create_eni(self, eni, attr_maps: dict):
+        self.app_dash_eni_table[str(eni)] = attr_maps
+
+    def remove_eni(self, eni):
+        del self.app_dash_eni_table[str(eni)]
+
+    def create_vnet_map(self, vnet, ip, attr_maps: dict):
+        self.app_dash_vnet_map_table[str(vnet) + ":" + str(ip)] = attr_maps
+
+    def remove_vnet_map(self, vnet, ip):
+        del self.app_dash_vnet_map_table[str(vnet) + ":" + str(ip)]
+
+    def create_outbound_routing(self, mac_string, ip, attr_maps: dict):
+        self.app_dash_route_table[str(mac_string) + ":" + str(ip)] = attr_maps
+
+    def remove_outbound_routing(self, mac_string, ip):
+        del self.app_dash_route_table[str(mac_string) + ":" + str(ip)]
+
+    def create_inbound_routing(self, mac_string, vni, ip, attr_maps: dict):
+        self.app_dash_route_rule_table[str(mac_string) + ":" + str(vni) + ":" + str(ip)] = attr_maps
+
+    def remove_inbound_routing(self, mac_string, vni, ip):
+        del self.app_dash_route_rule_table[str(mac_string) + ":" + str(vni) + ":" + str(ip)]
+
+
+class TestDash(object):
+    def test_appliance(self, dvs):
+        dashobj = Dash(dvs)
+        self.appliance_id = "100"
+        self.sip = "10.0.0.1"
+        self.vm_vni = "4321"
+        dashobj.create_appliance(self.appliance_id, {"sip": self.sip, "vm_vni": self.vm_vni})
+        time.sleep(3)
+
+        direction_entries = dashobj.asic_direction_lookup_table.get_keys()
+        assert direction_entries
+        fvs = dashobj.asic_direction_lookup_table[direction_entries[0]]
+        for fv in fvs:
+            if fv[0] == "SAI_DIRECTION_LOOKUP_ENTRY_ATTR_ACTION":
+                assert fv[1] == "SAI_DIRECTION_LOOKUP_ENTRY_ACTION_SET_OUTBOUND_DIRECTION"
+        vip_entries = dashobj.asic_vip_table.get_keys()
+        assert vip_entries
+        fvs = dashobj.asic_vip_table[vip_entries[0]]
+        for fv in fvs:
+            if fv[0] == "SAI_VIP_ENTRY_ATTR_ACTION":
+                assert fv[1] == "SAI_VIP_ENTRY_ACTION_ACCEPT"
+        return dashobj
+
+    def test_vnet(self, dvs):
+        dashobj = Dash(dvs)
+        self.vnet = "Vnet1"
+        self.vni = "45654"
+        self.guid = "559c6ce8-26ab-4193-b946-ccc6e8f930b2"
+        dashobj.create_vnet(self.vnet, {"vni": self.vni, "guid": self.guid})
+        time.sleep(3)
+        vnets = dashobj.asic_dash_vnet_table.get_keys()
+        assert vnets
+        self.vnet_oid = vnets[0]
+        vnet_attr = dashobj.asic_dash_vnet_table[self.vnet_oid]
+        assert vnet_attr["SAI_VNET_ATTR_VNI"] == "45654"
+        return dashobj
+
+    def test_eni(self, dvs):
+        dashobj = Dash(dvs)
+        self.vnet = "Vnet1"
+        self.mac_string = "F4939FEFC47E"
+        self.mac_address = "F4:93:9F:EF:C4:7E"
+        self.eni_id = "497f23d7-f0ac-4c99-a98f-59b470e8c7bd"
+        self.underlay_ip = "25.1.1.1"
+        self.admin_state = "enabled"
+        dashobj.create_eni(self.mac_string, {"eni_id": self.eni_id, "mac_address": self.mac_address, "underlay_ip": self.underlay_ip, "admin_state": self.admin_state, "vnet": self.vnet})
+        time.sleep(3)
+        vnets = dashobj.asic_dash_vnet_table.get_keys()
+        assert vnets
+        self.vnet_oid = vnets[0]
+        enis = dashobj.asic_eni_table.get_keys()
+        assert enis
+        self.eni_oid = enis[0];
+        fvs = dashobj.asic_eni_table[enis[0]]
+        for fv in fvs:
+            if fv[0] == "SAI_ENI_ATTR_VNET_ID":
+                assert fv[1] == str(self.vnet_oid)
+            if fv[0] == "SAI_ENI_ATTR_PPS":
+                assert fv[1] == 0
+            if fv[0] == "SAI_ENI_ATTR_CPS":
+                assert fv[1] == 0
+            if fv[0] == "SAI_ENI_ATTR_FLOWS":
+                assert fv[1] == 0
+            if fv[0] == "SAI_ENI_ATTR_ADMIN_STATE":
+                assert fv[1] == "true"
+
+        time.sleep(3)
+        eni_addr_maps = dashobj.asic_eni_ether_addr_map_table.get_keys()
+        assert eni_addr_maps
+        fvs = dashobj.asic_eni_ether_addr_map_table[eni_addr_maps[0]]
+        for fv in fvs:
+            if fv[0] == "SAI_ENI_ETHER_ADDRESS_MAP_ENTRY_ATTR_ENI_ID":
+                assert fv[1] == str(self.eni_oid)
+        return dashobj
+
+    def test_vnet_map(self, dvs):
+        dashobj = Dash(dvs)
+        self.vnet = "Vnet1"
+        self.ip = "10.1.1.1"
+        self.mac_address = "F4:93:9F:EF:C4:7E"
+        self.routing_type = "vnet_encap"
+        self.underlay_ip = "101.1.2.3"
+        dashobj.create_vnet_map(self.vnet, self.ip, {"mac_address": self.mac_address, "routing_type": self.routing_type, "underlay_ip": self.underlay_ip})
+        time.sleep(3)
+
+        vnet_ca_to_pa_maps = dashobj.asic_dash_outbound_ca_to_pa_table.get_keys()
+        assert vnet_ca_to_pa_maps
+        fvs = dashobj.asic_dash_outbound_ca_to_pa_table[vnet_ca_to_pa_maps[0]]
+        for fv in fvs:
+            if fv[0] == "SAI_OUTBOUND_CA_TO_PA_ENTRY_ATTR_UNDERLAY_DIP":
+                assert fv[1] == "101.1.2.3"
+            if fv[0] == "SAI_OUTBOUND_CA_TO_PA_ENTRY_ATTR_OVERLAY_DMAC":
+                assert fv[1] == "F9:22:83:99:22:A2"
+
+        vnet_pa_validation_maps = dashobj.asic_pa_validation_table.get_keys()
+        assert vnet_pa_validation_maps
+        fvs = dashobj.asic_pa_validation_table[vnet_pa_validation_maps[0]]
+        for fv in fvs:
+            if fv[0] == "SAI_PA_VALIDATION_ENTRY_ATTR_ACTION":
+                assert fv[1] == "SAI_PA_VALIDATION_ENTRY_ACTION_PERMIT"
+        return dashobj
+
+    def test_outbound_routing(self, dvs):
+        dashobj = Dash(dvs)
+        self.vnet = "Vnet1"
+        self.mac_string = "F4939FEFC47E"
+        self.ip = "10.1.0.0/24"
+        self.action_type = "vnet_direct"
+        self.overlay_ip= "10.0.0.6"
+        dashobj.create_outbound_routing(self.mac_string, self.ip, {"action_type": self.action_type, "overlay_ip": self.overlay_ip, "vnet": self.vnet})
+        time.sleep(3)
+
+        outbound_routing_entries = dashobj.asic_outbound_routing_table.get_keys()
+        assert outbound_routing_entries
+        fvs = dashobj.asic_outbound_routing_table[outbound_routing_entries[0]]
+        for fv in fvs:
+            if fv[0] == "SAI_OUTBOUND_ROUTING_ENTRY_ATTR_ACTION":
+                assert fv[1] == "SAI_OUTBOUND_ROUTING_ENTRY_ACTION_ROUTE_VNET_DIRECT"
+            if fv[0] == "SAI_OUTBOUND_ROUTING_ENTRY_ATTR_OVERLAY_IP":
+                assert fv[1] == "10.0.0.6"
+        return dashobj
+
+    def test_inbound_routing(self, dvs):
+        dashobj = Dash(dvs)
+        self.mac_string = "F4939FEFC47E"
+        self.vnet = "Vnet1"
+        self.vni = "3251"
+        self.ip = "10.1.1.1"
+        self.action_type = "decap"
+        self.pa_validation = "true"
+        self.priority = "1"
+        self.protocol = "0"
+        dashobj.create_inbound_routing(self.mac_string, self.vni, self.ip, {"action_type": self.action_type, "pa_validation": self.pa_validation, "priority": self.priority, "protocol": self.protocol, "vnet": self.vnet})
+        time.sleep(3)
+
+        inbound_routing_entries = dashobj.asic_inbound_routing_rule_table.get_keys()
+        assert inbound_routing_entries
+        fvs = dashobj.asic_inbound_routing_rule_table[inbound_routing_entries[0]]
+        for fv in fvs:
+            if fv[0] == "SAI_INBOUND_ROUTING_ENTRY_ATTR_ACTION":
+                assert fv[1] == "SAI_INBOUND_ROUTING_ENTRY_ACTION_VXLAN_DECAP_PA_VALIDATE"
+        return dashobj
+
+    def test_cleanup(self, dvs):
+        dashobj = Dash(dvs)
+        self.vnet = "Vnet1"
+        self.mac_string = "F4939FEFC47E"
+        self.vni = "3251"
+        self.sip = "10.1.1.1"
+        self.dip = "10.1.0.0/24"
+        self.appliance_id = "100"
+        dashobj.remove_inbound_routing(self.mac_string, self.vni, self.sip)
+        dashobj.remove_outbound_routing(self.mac_string, self.dip)
+        dashobj.remove_eni(self.mac_string)
+        dashobj.remove_vnet_map(self.vnet, self.sip)
+        dashobj.remove_vnet(self.vnet)
+        dashobj.remove_appliance(self.appliance_id)
+
+# Add Dummy always-pass test at end as workaroud
+# for issue when Flaky fail on final test it invokes module tear-down
+# before retrying
+def test_nonflaky_dummy():
+    pass
