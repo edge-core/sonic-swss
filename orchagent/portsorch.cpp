03b5b7986a981f0306dededa6d61f05cee36d9d1
mulin_huang <mulin_huang@edge-core.com>
2023-08-14 15:04:54 +0800

    [portsorch] Avoid the situation where the bridge port of VLAN memeber cannot be removed after VLAN removal
    In some cases, when vlan memeber has been remove, but bridge port is not removed beause that fdb_count is not zero.
    During the retry, the vlan is removed, and then the retry of removeBridgePort will not be executed.
    Therefore, In delete task of vlan member, it is not need to check vlan is existed.

--- orchagent/portsorch.cpp
+++ orchagent/portsorch.cpp
@@ -4565,13 +4565,6 @@ void PortsOrch::doVlanMemberTask(Consumer &consumer)
         assert(m_portList.find(vlan_alias) != m_portList.end());
         Port vlan, port;
 
-        /* When VLAN member is to be created before VLAN is created */
-        if (!getPort(vlan_alias, vlan))
-        {
-            SWSS_LOG_INFO("Failed to locate VLAN %s", vlan_alias.c_str());
-            it++;
-            continue;
-        }
 
         if (!getPort(port_alias, port))
         {
@@ -4582,6 +4575,14 @@ void PortsOrch::doVlanMemberTask(Consumer &consumer)
 
         if (op == SET_COMMAND)
         {
+            /* When VLAN member is to be created before VLAN is created */
+            if (!getPort(vlan_alias, vlan))
+            {
+                SWSS_LOG_INFO("Failed to locate VLAN %s", vlan_alias.c_str());
+                it++;
+                continue;
+            }
+
             string tagging_mode = "untagged";
 
             for (auto i : kfvFieldsValues(t))
@@ -4613,7 +4614,7 @@ void PortsOrch::doVlanMemberTask(Consumer &consumer)
         }
         else if (op == DEL_COMMAND)
         {
-            if (vlan.m_members.find(port_alias) != vlan.m_members.end())
+            if (getPort(vlan_alias, vlan) && vlan.m_members.find(port_alias) != vlan.m_members.end())
             {
                 if (removeVlanMember(vlan, port))
                 {
