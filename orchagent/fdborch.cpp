ea969353c03f3c8fe8fc693467687c364eb89c0e
iris_hsu <iris_hsu@edge-core.com>
2023-11-13 14:56:06 +0800

    [fdb] workaround for change mac type to dynamic.

--- orchagent/fdborch.cpp
+++ orchagent/fdborch.cpp
@@ -741,6 +741,36 @@ void FdbOrch::update(sai_fdb_event_t        type,
         }
         update.port.m_fdb_count++;
         m_portsOrch->setPort(update.port.m_alias, update.port);
+
+        if (existing_entry->second.origin == FDB_ORIGIN_MCLAG_ADVERTIZED || existing_entry->second.origin == FDB_ORIGIN_VXLAN_ADVERTIZED)
+        {
+            sai_status_t status;
+            sai_fdb_entry_t fdb_entry;
+            fdb_entry.switch_id = gSwitchId;
+            memcpy(fdb_entry.mac_address, entry->mac_address, sizeof(sai_mac_t));
+            fdb_entry.bv_id = entry->bv_id;
+            sai_attribute_t attr;
+            vector<sai_attribute_t> attrs;
+
+            attr.id = SAI_FDB_ENTRY_ATTR_TYPE;
+            attr.value.s32 = SAI_FDB_ENTRY_TYPE_DYNAMIC;
+            attrs.push_back(attr);
+
+            attr.id = SAI_FDB_ENTRY_ATTR_BRIDGE_PORT_ID;
+            attr.value.oid = bridge_port_id;
+            attrs.push_back(attr);
+
+            for(auto itr : attrs)
+            {
+                status = sai_fdb_api->set_fdb_entry_attribute(&fdb_entry, &itr);
+                if (status != SAI_STATUS_SUCCESS)
+                {
+                    SWSS_LOG_ERROR("macUpdate-Failed for MCLAG mac attr.id=0x%x for FDB %s in 0x%" PRIx64 "on %s, rv:%d",
+                                itr.id, update.entry.mac.to_string().c_str(), entry->bv_id, update.port.m_alias.c_str(), status);
+                }
+            }
+        }
+
         update.sai_fdb_type = SAI_FDB_ENTRY_TYPE_DYNAMIC;
         storeFdbEntryState(update);
 
