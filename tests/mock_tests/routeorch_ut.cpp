6e2b26ece9d6b8621ceedae2c64cd9d3d718eea5
minkang_tsai <minkang_tsai@edge-core.com>
2024-07-15 06:49:15 +0000

    JIRA-SONIC-10043: [Sync][routeorch] do not allow two entry in consumer with has the same prefix but different operation.
    * 86ad8210 -> JIRA-SONIC-9535: [routeorch] do not allow two entry in consumer with has the same prefix but different operation.
    * a17dce2d -> JIRA-SONIC-9535: [routeorch] modify unit test accorindg to SET/DEL behavior change

--- tests/mock_tests/routeorch_ut.cpp
+++ tests/mock_tests/routeorch_ut.cpp
@@ -360,8 +360,8 @@ namespace routeorch_test
 
         static_cast<Orch *>(gRouteOrch)->doTask();
         // Make sure both create and set has been called
-        ASSERT_EQ(current_create_count + 1, create_route_count);
-        ASSERT_EQ(current_remove_count + 1, remove_route_count);
+        ASSERT_EQ(current_create_count, create_route_count);
+        ASSERT_EQ(current_remove_count, remove_route_count);
         ASSERT_EQ(current_set_count, set_route_count);
 
         entries.clear();
@@ -397,9 +397,9 @@ namespace routeorch_test
 
         static_cast<Orch *>(gRouteOrch)->doTask();
         // Make sure both create and remove has been called
-        ASSERT_EQ(current_create_count + 1, create_route_count);
-        ASSERT_EQ(current_remove_count + 1, remove_route_count);
-        ASSERT_EQ(current_set_count, set_route_count);
+        ASSERT_EQ(current_create_count, create_route_count);
+        ASSERT_EQ(current_remove_count, remove_route_count);
+        ASSERT_EQ(current_set_count + 1, set_route_count);
     }
 
     TEST_F(RouteOrchTest, RouteOrchTestDelSetDefaultRoute)
