5ff7f4277bdc73c9eb24b3146a9c69329f884565
chaos_kao <chaos_kao@edge-core.com>
2025-02-06 01:22:28 +0000

    [Buffer] Fix the issue that applying buffer configration take long time during switch startup.
    - What I did:
      Fixed a bug to ensure that both PG and queue buffers are fully applied before port initialization begins.
    
    - Why I did:
      Due to a code defect, the ports start initializing once the buffer is applied only to the queue rather than waiting for the entire buffer to be applied. This means extra time is needed to complete applying the buffer to the PG, resulting in the switch taking 3 minutes to fully initialize its ports at startup.
    
    - How I verified:
      Tested the fix on the AS4630-54NPE switch. With the fix applied, port initialization completed in 9 seconds. Without the fix, it took 3 minutes and 37 seconds.

--- orchagent/bufferorch.h
+++ orchagent/bufferorch.h
@@ -28,6 +28,8 @@ const string buffer_value_egress            = "egress";
 const string buffer_value_both              = "both";
 const string buffer_profile_list_field_name = "profile_list";
 const string buffer_headroom_type_field_name= "headroom_type";
+const string buffer_pg_prefix               = "PG_";
+const string buffer_queue_prefix            = "QUEUE_";
 
 class BufferOrch : public Orch
 {
