d6fed846fed334ae132ea94f167dadd0304dc84a
junying_yeh <junying_yeh@edge-core.com>
2026-02-06 11:06:26 +0800

    Add dhcp_l2 and dhcpv6_l2 trap.

--- orchagent/copporch.cpp
+++ orchagent/copporch.cpp
@@ -88,7 +88,9 @@ static map<string, sai_hostif_trap_type_t> trap_id_map = {
     {"dest_nat_miss", SAI_HOSTIF_TRAP_TYPE_DNAT_MISS},
     {"ldp", SAI_HOSTIF_TRAP_TYPE_LDP},
     {"bfd_micro", SAI_HOSTIF_TRAP_TYPE_BFD_MICRO},
-    {"bfdv6_micro", SAI_HOSTIF_TRAP_TYPE_BFDV6_MICRO}
+    {"bfdv6_micro", SAI_HOSTIF_TRAP_TYPE_BFDV6_MICRO},
+    {"dhcp_l2", SAI_HOSTIF_TRAP_TYPE_DHCP_L2},
+    {"dhcpv6_l2", SAI_HOSTIF_TRAP_TYPE_DHCPV6_L2},
 };
 
 
